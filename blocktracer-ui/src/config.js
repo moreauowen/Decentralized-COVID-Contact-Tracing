d24e07d4f7132b2ea441e75dacd1da772e599482
Owen Moreau <moreauowen@gmail.com>
2020-12-02 14:31:21 -0500

    Refactor to be more clear

--- blocktracer-ui/src/config.js
+++ blocktracer-ui/src/config.js
@@ -1,10 +1,11 @@
 import Web3 from 'web3';
 const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545/"));
 
-// TODO: fill these with contract details
-let account0 = '0x92d7E6838788e620A8de81d06CF557E356E0175D';
-let blockTracerABI = [{"constant":false,"inputs":[{"internalType":"string","name":"fullName","type":"string"},{"internalType":"string","name":"phone","type":"string"},{"internalType":"string","name":"location","type":"string"}],"name":"startTraceChain","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"inviteToChain","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"}];
-let blockTracerAddress = '0xfBB34cecC68F4A27E3C27608946eb1D7Db4AFA40';
+// NOTE - These values are gonna change each time depending on testing environment
+let account0 = '0x897E3EdcEea0A27969D5145D6F9bbC037E2000B7';
+let blockTracerABI = [{"constant":false,"inputs":[{"internalType":"string","name":"fullName","type":"string"},{"internalType":"string","name":"phone","type":"string"},{"internalType":"string","name":"location","type":"string"}],"name":"startTraceChain","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"internalType":"string","name":"fullName","type":"string"},{"internalType":"string","name":"phone","type":"string"},{"internalType":"string","name":"location","type":"string"}],"name":"testMap","outputs":[{"internalType":"string","name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"inviteToChain","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"}];
+let blockTracerAddress = '0x2D5C99220A9fab5D5928D753E69EdB45D25E6aD2';
 
+// Export to use in React app
 const blockTracerContract = new web3.eth.Contract(blockTracerABI, blockTracerAddress);
 export { blockTracerContract, account0};
