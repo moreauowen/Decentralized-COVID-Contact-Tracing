78a29cf9e70a4e74cee3e0738281959059f6898d
Owen Moreau <moreauowen@gmail.com>
2020-12-02 14:31:33 -0500

    Fix form submission to use contract function

--- blocktracer-ui/src/components/StartTrace.js
+++ blocktracer-ui/src/components/StartTrace.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { blockTracerContract, account0 } from '../config';
+
 
 class StartTrace extends Component {
     constructor(props) {
@@ -8,61 +10,69 @@ class StartTrace extends Component {
             phoneNumber: '',
             location: '',
             testResults: '',
+            success: false
         }
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleChange(event) {
-        this.setState({
-            fullName: event.target.fullName,
-            phoneNumber: event.target.phoneNumber,
-            location: event.target.location,
-            testResults: event.target.testResults
-        });
+    handleChange = e => {
+        this.setState({ [e.target.id]: e.target.value })
     }
 
     handleSubmit(event) {
-        console.log(this.state);
         event.preventDefault();
+
+        blockTracerContract.methods.startTraceChain(this.state.fullName, this.state.phoneNumber, this.state.location)
+        .send({ from: account0, gas: 150000 }, (err, transactionHash) => {
+          if (!err) {
+            this.setState({
+              success: true
+            });
+          } else {
+              console.log(err);
+          }
+        })
+
     }
 
-
+    // TODO - Can you please style the part at the bottom of the form that shows whether or not the submission was successful
     render() {
         return (
             <div id="startTrace">
                 <h2>Start a trace investigation...</h2>
                 <br></br>
-                <form className="traceform" classonSubmit={this.handleSubmit}>
+                <form className="traceform" onSubmit={this.handleSubmit}>
                     <label>
                         Full Name
                         <br></br>
-                        <input type="text" value={this.state.fullName} onChange={this.handleChange} />
+                        <input type="text" id='fullName' value={this.state.fullName} onChange={this.handleChange} />
                     </label>
                     <br></br>
                     <label>
                         Phone Number
                         <br></br>
-                        <input type="text" value={this.state.phoneNumber} onChange={this.handleChange} />
+                        <input type="text" id='phoneNumber' value={this.state.phoneNumber} onChange={this.handleChange} />
                     </label>
                     <br></br>
                     <label>
                         Location
                         <br></br>
-                        <input type="text" value={this.state.location} onChange={this.handleChange} />
+                        <input type="text" id='location' value={this.state.location} onChange={this.handleChange} />
                     </label>
                     <br></br>
-                    <lable>
+                    <label>
                         Test Results
                         <br></br>
-                        <input type="text" value={this.state.testResults} onChange={this.handleChange} />
-                    </lable>
+                        <input type="text" id='testResults' value={this.state.testResults} onChange={this.handleChange} />
+                    </label>
                     <br></br>
                     <div id="startBtnParent">
                         <input id="startBtn" type="submit" value="Start Tracing"/>
                     </div>
                 </form>
+                {this.state.success ? <h3>Success</h3> : <h3>Not sent yet</h3>}
             </div>
         );
     }
